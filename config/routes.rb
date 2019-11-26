9684ada27ca81dec92f4a9dfbb46690963837a89
huifang boo <huifangb@gmail.com>
2019-11-26 18:28:20 +0800

    nested booking under events in routes.rb, created bookings controller and new booking action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   root to: "events#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events
+  resources :events do
+    resources :bookings, only: [ :new, :create, :show ]
+  end
+  resources :bookings, only: [ :edit, :update, :destroy]
+
 end
