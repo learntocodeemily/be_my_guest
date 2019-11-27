f540c9549788792f4a4e8732fb85bee5b2db0a3e
emilyzhao <51254444+learntocodeemily@users.noreply.github.com>
2019-11-27 14:18:25 +0800

    comment back the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,26 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
-  # include Pundit   # add this when setup pundit
+  before_action :authenticate_user!
+  include Pundit   # add this when setup pundit
 
-  # # coldes below were adding a first_name and Last_name at sign up
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  # coldes below were adding a first_name and Last_name at sign up
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # # those two afetr_action code is pudit:white-list approach
-  # after_action :verify_authorized, except: [:index, :show], unless: :skip_pundit?
-  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  # those two afetr_action code is pudit:white-list approach
+  after_action :verify_authorized, except: [:index, :show], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-  # def configure_permitted_parameters
-  #   # For additional fields in app/views/devise/registrations/new.html.erb
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
 
-  #   # For additional in app/views/devise/registrations/edit.html.erb
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
-  # end
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 
-  # private
+  private
 
-  # def skip_pundit?
-  #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
-  # end
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 
 end
