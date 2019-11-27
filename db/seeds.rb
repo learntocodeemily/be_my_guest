a73c59fe95f0989e655c5a613eda28df897f12d2
huifang boo <huifangb@gmail.com>
2019-11-27 13:21:30 +0800

    fixed heroku error msgs

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Review.destroy_all
+Booking.destroy_all
 Event.destroy_all
 User.destroy_all
 
