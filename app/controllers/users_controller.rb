ab3e369842f5fea6f470c320b9bf1843805946e2
huifang boo <huifangb@gmail.com>
2019-11-28 21:29:02 +0800

    added user profiling page and user info display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [ :show, :edit, :update, ]
+  # skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
 
 
@@ -8,12 +11,39 @@ class UsersController < ApplicationController
     @events = current_user.events
     @bookings = current_user.bookings
     @eventcount = @events.count
-  end
-
-  def new
+    @name = "#{current_user.first_name} #{current_user.last_name}"
   end
 
   def edit
+    # authorize @user
   end
+
+  def update
+    # authorize @user
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  # def destroy
+  #   authorize @user
+  #   @user.destroy
+
+  #   redirect_to user_path
+  # end
+
+
+  private
+
+  def find_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:avatar, :diet, :allergy)
+  end
+
 
 end
