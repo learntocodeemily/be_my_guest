9d01abc6d812cb438dc71729814f7cb43f530fc4
huifang boo <huifangb@gmail.com>
2019-11-28 19:38:09 +0800

    empty commit for style

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,4 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
-// comment for commit
+// comment for commitjhgasjhd
 flatpickr(".datepicker", {})
