8054d196fb6aaa5e6dd5ed84b36c3f812ba33b6c
huifang boo <huifangb@gmail.com>
2019-11-29 15:10:00 +0800

    added star rating for reviews

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox.js';
 import "../plugins/flatpickr"
 initMapbox();
+
+import { initStarRating } from '../plugins/init_star_rating';
+
+initStarRating();
